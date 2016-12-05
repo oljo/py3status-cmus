5f372c9f830adf48c72e348898eb6715bdb27bc0
oljo <oljo.dev@gmail.com>
2016-12-05 20:45:18 +0200

    Changed static methods to instance methods. 	- Added possibility to configure variables via py3status config 	- Added playtime

--- cmus_status.py
+++ cmus_status.py
@@ -12,8 +12,9 @@ from subprocess import Popen, PIPE
 # STYLING #
 ###########
  
-STATUS_OUTPUT_FORMAT = "%status% %artist% - %tracknumber%. %title%"
+OUTPUT_FORMAT = "%status% %artist% - %tracknumber%. %title%"
 CMUS_NOT_RUNNING_MSG = ""
+CACHE_TIMEOUT = 0.5
 NOTHING_PLAYING_MSG = "Nothing playing."
 SHOW_PROGRESS_BAR = True
 PROGRESS_BAR_STYLE = {1: u"█",
@@ -35,8 +36,7 @@ STOP_SYM  = u"◻"
 ########################
 
 CMUS_CMD  = "cmus-remote"
-CMUS_ARGS = ["-Q"]
-CMUS_STATUS_CMD = [CMUS_CMD] + CMUS_ARGS
+CMUS_ARGS = "-Q"
 CMUS_STATUS_MAP = {"playing" : PLAY_SYM,
                    "stopped" : STOP_SYM,
                    "paused"  : PAUSE_SYM
@@ -47,108 +47,34 @@ CMUS_STATUS_MAP = {"playing" : PLAY_SYM,
 # 
 # xev can be used to check the mouse button numbers.
 
-CACHE_UNTIL = 0.5 # In seconds
 ON_CLICK_MAP = {1 : [CMUS_CMD] + ["-u"], # Play/pause
                 9 : [CMUS_CMD] + ["-n"], # Next track
                 8 : [CMUS_CMD] + ["-r"]  # Previous track
                 }
-def main():
-    cmus_status = {"sets" : {},
-                   "tags" : {}
-                  }
 
-    try:
-        cmusProcess = Popen(CMUS_STATUS_CMD, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        output, error = cmusProcess.communicate()
-        error = error.decode("utf-8")
-    except FileNotFoundError:
-        cmus_status["status"] = "cmus-remote not found."
-        return cmus_status
-       
-    if error:
-        if "not running" in error:
-             cmus_status["status"] = CMUS_NOT_RUNNING_MSG
-        else:
-             cmus_status["status"] = error
-        return cmus_status 
-
-    cmus_remote_output = output.decode("utf-8")
-    
-    cmus_remote_output = cmus_remote_output.splitlines()
-    cmus_remote_output = [x.split(" ", 1) for x in cmus_remote_output]
-
-    for output in cmus_remote_output:
-        if "tag" in output:
-             tmpList = output[1].split(" ", 1)
-             tmpDict = dict(zip([tmpList[0]], [tmpList[1]]))
-             cmus_status["tags"].update(tmpDict)
-        elif "set" in output:
-             tmpList = output[1].split(" ", 1)
-             tmpDict = dict(zip([tmpList[0]], [tmpList[1]]))
-             cmus_status["sets"].update(tmpDict)
-        else:
-             if "status" in output:
-                 output[1] = CMUS_STATUS_MAP[output[1]]
-             cmus_status.update(dict(zip([output[0]], [output[1]])))
-    return cmus_status
-
-def get_progress_bar(input_data):
-    try:
-        duration = float(input_data["duration"])
-        position = float(input_data["position"])
-        progress = position/duration
-        
-        progress_str = []
-        for i in range(PROGRESS_BAR_LENGTH):
-            if i < round(progress*PROGRESS_BAR_LENGTH):
-                 progress_str.append(PROGRESS_BAR_STYLE[1])
-            else:
-                 progress_str.append(PROGRESS_BAR_STYLE[2])
-
-        progress_str = "".join(progress_str)
-        return progress_str
-
-    except:
-        return ""
-
-def format_status_output(input_data):
-    status_output = ""
-    cmus_status = input_data
-    status_vars = re.findall(r'%(\w+)%', STATUS_OUTPUT_FORMAT)
-    tmp_status_output = STATUS_OUTPUT_FORMAT
-    for status_var in status_vars:
-         if status_var in cmus_status:
-             status_var = status_var
-             try:
-                 status = cmus_status[status_var]
-             except:
-                 status = status_var
-             if status == CMUS_NOT_RUNNING_MSG:
-                 return status
-             tmp_status_output = tmp_status_output.replace("%" + status_var + "%", status)
-         elif status_var in cmus_status["tags"]:
-             tmp_status_output = tmp_status_output.replace("%" + status_var + "%", cmus_status["tags"][status_var])
-
-         elif status_var in cmus_status["sets"]:
-             tmp_status_output = mp_status_output.replace("%" + status_var + "%", cmus_status["sets"][status_var])
-
-    if not len(cmus_status["tags"]):
-        if cmus_status["status"] in ["stopped", CMUS_STATUS_MAP["stopped"]]:
-            return NOTHING_PLAYING_MSG
-        elif not cmus_status["status"] in ["playing", "paused"]:
-            return cmus_status["status"]
-
-    status_output = re.sub(r'%(\w+)%', '', tmp_status_output)
-    return status_output
 
 class Py3status:
+    cache_timeout = CACHE_TIMEOUT
+    output_format = OUTPUT_FORMAT
+    show_progress_bar = SHOW_PROGRESS_BAR
+    progress_bar_length = PROGRESS_BAR_LENGTH
+    not_running_msg = CMUS_NOT_RUNNING_MSG
+    nothing_playing_msg = NOTHING_PLAYING_MSG
+    cmus_args = CMUS_ARGS
+    cmus_args = cmus_args.split()    
+    show_playtime = True
+
     def status(self):
-        status_out_dict = main()
-        status_output = format_status_output(status_out_dict)
-        if SHOW_PROGRESS_BAR:
-             status_output += " " + get_progress_bar(status_out_dict)
+        status_out_dict = self._main()
+        status_output = self._format_status_output(status_out_dict)
+        if self.show_progress_bar:
+             status_output += " " + self._get_progress_bar(status_out_dict)
+        if self.show_playtime:
+            playtime = self._get_playtime(status_out_dict)
+            if playtime:
+                status_output += " " + playtime
         return {"full_text" : status_output,
-                "cached_until" : self.py3.time_in(CACHE_UNTIL)}
+                "cached_until" : self.py3.time_in(self.cache_timeout)}
     
     def on_click(self, event):
         button = event["button"]
@@ -157,6 +83,111 @@ class Py3status:
             output, error = cmusProcess.communicate()
             error = error.decode()
 
+    def _get_playtime(self, input_data):
+        playtime_str = ""
+        try:
+            position = float(input_data["position"])
+        except:
+            return ""
+        m, s = divmod(position, 60)
+        h, m = divmod(m, 60)
+        if h:
+             playtime_str += " %d:%02d:%02d" % (h, m, s)
+        else:
+             playtime_str += " %02d:%02d" % (m, s)
+        return playtime_str
+
+
+    def _main(self):
+        cmus_status = {"sets" : {},
+                       "tags" : {}
+                      }
+    
+        try:
+            cmus_status_cmd = [CMUS_CMD] + self.cmus_args
+            cmusProcess = Popen(cmus_status_cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            output, error = cmusProcess.communicate()
+            error = error.decode("utf-8")
+        except FileNotFoundError:
+            cmus_status["status"] = "cmus-remote not found."
+            return cmus_status
+           
+        if error:
+            if "not running" in error:
+                 cmus_status["status"] = self.not_running_msg
+            else:
+                 cmus_status["status"] = error
+            return cmus_status 
+    
+        cmus_remote_output = output.decode("utf-8")
+        
+        cmus_remote_output = cmus_remote_output.splitlines()
+        cmus_remote_output = [x.split(" ", 1) for x in cmus_remote_output]
+    
+        for output in cmus_remote_output:
+            if "tag" in output:
+                 tmpList = output[1].split(" ", 1)
+                 tmpDict = dict(zip([tmpList[0]], [tmpList[1]]))
+                 cmus_status["tags"].update(tmpDict)
+            elif "set" in output:
+                 tmpList = output[1].split(" ", 1)
+                 tmpDict = dict(zip([tmpList[0]], [tmpList[1]]))
+                 cmus_status["sets"].update(tmpDict)
+            else:
+                 if "status" in output:
+                     output[1] = CMUS_STATUS_MAP[output[1]]
+                 cmus_status.update(dict(zip([output[0]], [output[1]])))
+        return cmus_status
+    
+    def _get_progress_bar(self, input_data):
+        try:
+            duration = float(input_data["duration"])
+            position = float(input_data["position"])
+            progress = position/duration            
+
+            progress_str = []
+            for i in range(self.progress_bar_length):
+                if i < round(progress*self.progress_bar_length):
+                     progress_str.append(PROGRESS_BAR_STYLE[1])
+                else:
+                     progress_str.append(PROGRESS_BAR_STYLE[2])
+    
+            progress_str = "".join(progress_str)
+            return progress_str
+    
+        except:
+            return ""
+
+    def _format_status_output(self, input_data):
+        status_output = ""
+        cmus_status = input_data
+        status_vars = re.findall(r'%(\w+)%', self.output_format)
+        tmp_status_output = self.output_format
+        for status_var in status_vars:
+             if status_var in cmus_status:
+                 status_var = status_var
+                 try:
+                     status = cmus_status[status_var]
+                 except:
+                     status = status_var
+                 if status == self.not_running_msg:
+                     return status
+                 tmp_status_output = tmp_status_output.replace("%" + status_var + "%", status)
+             elif status_var in cmus_status["tags"]:
+                 tmp_status_output = tmp_status_output.replace("%" + status_var + "%", cmus_status["tags"][status_var])
+    
+             elif status_var in cmus_status["sets"]:
+                 tmp_status_output = mp_status_output.replace("%" + status_var + "%", cmus_status["sets"][status_var])
+    
+        if not len(cmus_status["tags"]):
+            if cmus_status["status"] in ["stopped", CMUS_STATUS_MAP["stopped"]]:
+                return self.nothing_playing_msg
+            elif not cmus_status["status"] in ["playing", "paused"]:
+                return cmus_status["status"]
+    
+        status_output = re.sub(r'%(\w+)%', '', tmp_status_output)
+        return status_output
+
 if __name__ == "__main__":
-   print(format_status_output(main()))
-   print(get_progress_bar(main()))
+     from py3status.module_test import module_test
+     module_test(Py3status)
